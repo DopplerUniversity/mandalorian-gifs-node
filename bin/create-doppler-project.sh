8cd10fe90acd3d0010b0c81aab91f35143282260
Ryan Blunden <ryan.blunden@gmail.com>
2022-11-30 22:29:44 +1000

    Minor updates

--- bin/create-doppler-project.sh
+++ bin/create-doppler-project.sh
@@ -9,8 +9,8 @@ doppler import
 doppler setup --no-interactive --silent
 
 echo -e '\n[info]: Tweaking staging and production values'
-doppler secrets delete HOST PORT DEBUG_COLORS --config stg -y --silent
-doppler secrets delete HOST PORT DEBUG_COLORS --config prd -y --silent
+doppler secrets delete HOSTNAME PORT DEBUG_COLORS --config stg -y --silent
+doppler secrets delete HOSTNAME PORT DEBUG_COLORS --config prd -y --silent
 
 echo -e '\n[info]: Opening the Doppler dashboard'
 sleep 1
